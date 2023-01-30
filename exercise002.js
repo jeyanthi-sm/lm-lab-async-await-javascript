5d27e0e85d9930033b2d85afb049f94ef97fcfa6
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-01-30 19:37:59 +0000

    Updated FetchData with async and await and also changed the top level function as async

--- exercise002.js
+++ exercise002.js
@@ -1,28 +1,20 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const tryApi = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=df193315a4981dd6efc272f5e432ca6b";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
-
-fetchData(jsonTypicode);
-
-fetchDetails(jsonTypicode);
-async function fetchDetails(input) {
+const fetchData = async (apiEndPoint) => {
   try {
-  const responsedet = await fetch(input);  
-  if (responsedet.ok) {
-  const responsedata = await responsedet.json();
-  console.log(responsedata);
+    const responsedet = await fetch(apiEndPoint);  
+    if (responsedet.ok) {
+    const responsedata = await responsedet.json();
+    console.log(responsedata);
+    }
   }
-}
-  catch (err) {
-    console.log(err);
+    catch (err) {
+      console.log(err);
+    }
   }
-}
-
-fetchDetails('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=df193315a4981dd6efc272f5e432ca6b');
+  
+fetchData(jsonTypicode);
+fetchData(tryApi);
